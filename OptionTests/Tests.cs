034e87f1a0fa2be2054cc1bd59289c42e2d592fb
Daniel Nixon <dan.nixon@gmail.com>
2014-09-01 16:27:49 +1000

    More tests.

--- OptionTests/Tests.cs
+++ OptionTests/Tests.cs
@@ -1,12 +1,43 @@
 ﻿using NUnit.Framework;
 using OptionType;
 using System;
+using System.Linq;
 
 namespace OptionTypeTests
 {
 	[TestFixture]
 	public class Tests
 	{
+		[Test]
+		public void AllNonEmpty()
+		{
+			var foo = "foo".ToOption();
+			Assert.IsTrue(foo.All(x => x.StartsWith("f")));
+			Assert.IsFalse(foo.All(x => x.StartsWith("a")));
+		}
+
+		[Test]
+		public void AllEmpty()
+		{
+			Option<string> foo = Option.Empty;
+			Assert.IsTrue(foo.All(x => x.StartsWith("f")));
+		}
+
+		[Test]
+		public void AnyNonEmpty()
+		{
+			var foo = "foo".ToOption();
+			Assert.IsTrue(foo.Any(x => x.StartsWith("f")));
+			Assert.IsFalse(foo.All(x => x.StartsWith("a")));
+		}
+
+		[Test]
+		public void AnyEmpty()
+		{
+			Option<string> foo = Option.Empty;
+			Assert.IsFalse(foo.Any(x => x.StartsWith("f")));
+		}
+
 		[Test]
 		public void FoldNonEmpty()
 		{
@@ -23,6 +54,67 @@ namespace OptionTypeTests
 			var expected = "bar";
 			var actual = foo.Fold(() => "bar", r => r);
 			Assert.AreEqual(expected, actual);
+		}
+
+		[Test]
+		public void HasValueNonEmpty()
+		{
+			var foo = "foo".ToOption();
+			Assert.IsTrue(foo.HasValue);
+		}
+
+		[Test]
+		public void HasValueEmpty()
+		{
+			Option<string> foo = Option.Empty;
+			Assert.IsFalse(foo.HasValue);
+		}
+
+		[Test]
+		public void SelectNonEmpty()
+		{
+			var foo = 1.ToOption();
+			var expected = 2.ToOption();
+			Assert.AreEqual(expected, foo.Select(x => x + 1));
+		}
+
+		[Test]
+		public void SelectEmpty()
+		{
+			Option<int> foo = Option.Empty;
+			Option<int> expected = Option.Empty;
+			Assert.AreEqual(expected, foo.Select(x => x + 1));
+		}
+
+		[Test]
+		public void ValueNonEmpty()
+		{
+			var foo = "foo".ToOption();
+			Assert.AreEqual("foo", foo.Value);
+		}
+
+		[Test]
+		public void ValueEmpty()
+		{
+			Option<string> foo = Option.Empty;
+			Assert.Throws<InvalidOperationException>(delegate { var v = foo.Value; });
+		}
+
+		[Test]
+		public void WhereNonEmpty()
+		{
+			var foo = "foo".ToOption();
+			Option<string> empty = Option.Empty;
+			Assert.AreEqual(foo, foo.Where(x => x.StartsWith("f")));
+			Assert.AreEqual(empty, foo.Where(x => x.StartsWith("a")));
+		}
+
+		[Test]
+		public void WhereEmpty()
+		{
+			Option<string> foo = Option.Empty;
+			Option<string> empty = Option.Empty;
+			Assert.AreEqual(empty, foo.Where(x => x.StartsWith("a")));
 		}
 	}
 }
